6c4fbc56c14be73a240fb09c89c559f629317223
cobaltblu27 <cobaltblu27@gmail.com>
2020-02-15 19:03:08 +0900

    add babel plugin-transform-runtime(enable async)

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "./FileInput.scss";
 import { decodeAudio } from "../lib/audio";
-// import * as RsAudio from "pkg";
+// import * as RsAudio from "rs-module";
 
 const FileInput = (props: { setAudio: (blob: Blob) => void }) => {
   // RsAudio.greet();
-  // const handleDrop = async (files: FileList | null, event: any) => {
-  //   if (files == null) return;
-  //   if (files.length !== 1) {
-  //     alert("파일을 한개만 놓으세요.");
-  //     return;
-  //   }
-  //   try {
-  //     const audioBlob = await decodeAudio(files[0]);
-  //     props.setAudio(audioBlob);
-  //   } catch (e) {
-  //     alert(e.message);
-  //   }
-  // };
+
+  const handleDrop = async (files: FileList | null, event: any) => {
+    if (files == null) return;
+    if (files.length !== 1) {
+      alert("파일을 한개만 놓으세요.");
+      return;
+    }
+    try {
+      const audioBlob = await decodeAudio(files[0]);
+      props.setAudio(audioBlob);
+    } catch (e) {
+      alert(e.message);
+    }
+  };
 
   return (
     // <div className="file-input" onDrop={handleDrop}>
