cd5ef7e3ea145cb3fe4f88dc132f0858636ef26d
cobaltblu27 <cobaltblu27@gmail.com>
2020-02-15 20:53:32 +0900

    add react-file-drop

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { ReactEventHandler } from "react";
 import "./FileInput.scss";
-import { decodeAudio } from "../lib/audio";
-import("rs-module").then(module => {
-  module.greet();
-});
+import AudioDecoder from "../lib/AudioDecoder";
+import FileDrop from "react-file-drop";
 
 const FileInput = (props: { setAudio: (blob: Blob) => void }) => {
+  const decoder = new AudioDecoder();
+
   const handleDrop = async (files: FileList | null, event: any) => {
     if (files == null) return;
     if (files.length !== 1) {
@@ -13,7 +13,7 @@ const FileInput = (props: { setAudio: (blob: Blob) => void }) => {
       return;
     }
     try {
-      const audioBlob = await decodeAudio(files[0]);
+      const audioBlob = await decoder.decode(files[0]);
       props.setAudio(audioBlob);
     } catch (e) {
       alert(e.message);
@@ -21,14 +21,13 @@ const FileInput = (props: { setAudio: (blob: Blob) => void }) => {
   };
 
   return (
-    // <div className="file-input" onDrop={handleDrop}>
-    <div className="file-input">
+    <FileDrop className="file-input" onDrop={handleDrop}>
       drop .mp3 here
       <div className="url-input-container">
         <input placeholder="..or enter youtube url here."></input>
         <button>enter</button>
       </div>
-    </div>
+    </FileDrop>
   );
 };
 
---
+++ src/lib/audioDecoder.ts
@@ -0,0 +1,52 @@
+import { AudioType, TODO } from './types'
+
+class AudioDecoder {
+  private rsAudioModule: any = undefined
+  private loadPromise?: Promise<any> = undefined
+
+  private static _singleTon?: AudioDecoder = undefined
+
+  constructor() {
+    if (AudioDecoder._singleTon != null) {
+      return AudioDecoder._singleTon
+    }
+    this.loadModule()
+  }
+
+  private loadModule = () => {
+    this.loadPromise = import('rs-module')
+      .then(m => this.rsAudioModule = m)
+  }
+
+  public decode = async (file: File): Promise<Blob> => {
+    const extension: string = file.name.split(".").pop()!
+    if (
+      !Object.values(AudioType)
+        .map(t => t as string)
+        .includes(extension)
+    ) {
+      throw Error(".mp3, .wav 파일만 사용 가능합니다")
+    }
+    const audioType = extension as AudioType
+
+    if (this.rsAudioModule == null) {
+      await this.loadPromise
+    }
+
+    switch (audioType) {
+      case AudioType.MP3:
+        return await this.rsAudioModule.decodeMp3(file as Blob)
+      case AudioType.WAV:
+        return await this.rsAudioModule.decodeWav(file as Blob)
+    }
+  }
+
+  public async greet() {
+    if (this.rsAudioModule == null) {
+      await this.loadPromise
+    }
+    this.rsAudioModule.greet()
+  }
+}
+
+export default AudioDecoder;
