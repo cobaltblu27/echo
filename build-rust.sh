a53b544d704b8c2cc6ef562bbbf8633b8914e68f
cobaltblu27 <cobaltblu27@gmail.com>
2020-02-16 16:44:09 +0900

    build-rust 수정

--- build-rust.sh
+++ build-rust.sh
@@ -3,21 +3,17 @@
 set -e
 
 # nabbed from SO; cd to directory project lives in:
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RS_PKG_NAME="rust"
+echo $DIR
 
-# update (in release mode) the wasm:
+cd $DIR/rs
 # cargo build --release --target wasm32-unknown-unknown
-
-# # update the wasm bindings:
-# wasm-bindgen target/wasm32-unknown-unknown/release/wasm_fractal.wasm \
-#     --no-modules \
-#     --no-modules-global wasmFractal \
-#     --no-typescript \
-#     --out-dir js
-cd rs
 wasm-pack build --out-dir rs-pkg 
 
-cd ..
-yarn remove rust
+rust_pkg=`yarn list --pattern $RS_PKG_NAME --depth=0 --silent`
+if [ ! -z "${rust_pkg}"  ]; then
+  yarn remove $RS_PKG_NAME
+fi
 yarn add file:./rs/rs-pkg
 yarn install
